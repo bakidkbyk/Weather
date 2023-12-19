88ad209a016e1211a29b010d9fccf8a1d3f8f5c7
bakidkbyk <baki.ordulu@gmail.com>
2023-12-19 07:16:57 +0300

    add swiftgen

---
+++ UIComponents/UIComponents/Resources/Strings/Localize.swift
@@ -0,0 +1,14 @@
+//
+//  Localize.swift
+//  UIComponents
+//
+//  Created by Baki Dikbıyık on 19.12.2023.
+//
+
+public class Localize {
+    
+    public static let shared = Localize()
+    public var strings: [String: String] = [:]
+    
+    public init() {}
+}
